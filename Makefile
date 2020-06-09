f487fa86227b3e79b6c777b9368eb5a163768642
Simen A. W. Olsen <cobraz@cobraz.no>
2020-06-09 13:42:34 +0200

    Implement new Tripletex Swagger file

--- Makefile
+++ Makefile
@@ -1,12 +1,29 @@
 generate:
-	openapi-generator \
+	java -Xmx4G -jar openapi-generator-cli.jar \
 	generate \
-	-i ./out/openapi.json \
+	-i ./swagger-fixed.json \
 	-g typescript-fetch \
 	-o ./out -v
 
+generate-debug:
+	java -Xmx4G -jar openapi-generator-cli.jar \
+	generate -debugModels -debugOperations \
+	-i https://gist.githubusercontent.com/cobraz/e46754a820e837862afa70e0851f7b00/raw/438b82b13c3c724b106f84ba375a89a612df532a/tripletex-swagger.yaml \
+	-g typescript-fetch \
+	-o ./out -v
+
+generate-docker:
+	docker run -v "${PWD}:/local" openapitools/openapi-generator-cli:latest \
+	generate \
+	-i /local/swagger-fixed.json \
+	-g typescript-node \
+	-o /local/out
+
+download_jars:
+	curl -o ./openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.3.1/openapi-generator-cli-4.3.1.jar
+
 download_swagger:
-	curl -o ./swagger.json https://tripletex.no/v2/swagger.json
+	curl -o ./swagger.json https://triplepub.s3-eu-west-1.amazonaws.com/swagger.json
 
 .PHONY: download_swagger
 .PHONY: generate
--- scripts/fixSwagger.js
+++ scripts/fixSwagger.js
@@ -2,45 +2,69 @@
 
 const fs = require('fs');
 const camelCase = require('camelcase');
+const transform = require('js-object-transform');
+const sway = require('sway');
 
-const convertName = (url, func, withParam = false) => {
-  let safeUrlName = url.replace(/\/|\<|:|\>/gi, ' ').replace('<', ' ');
-  if (!safeUrlName.match(/\{/) && func === 'get') {
-    func = 'list';
-    safeUrlName = safeUrlName.replace(/\{id\}/gm, '').replace(/\{(.*)\}/gm, '');
-  } else {
-    if (withParam) {
-      safeUrlName = safeUrlName.replace(/\{(.*)\}/gm, '$1');
-    } else {
-      safeUrlName = safeUrlName.replace(/\{(.*)\}/gm, '');
-    }
-  }
-  return `${func} ${safeUrlName}`.replace('put', 'update').replace('post', 'create');
+const validate = async (swaggerFile) => {
+  const api = await sway.create({
+    definition: swaggerFile,
+  });
+
+  return api.validate();
 };
 
 (async () => {
   const rawSwaggerFile = fs.readFileSync(`${__dirname}/../swagger.json`);
   const swaggerFile = JSON.parse(rawSwaggerFile);
 
-  let generatedOperationIds = [];
+  // Fix issue with /ledger/voucher/{id}/attachment
+  const deleteIssue = swaggerFile.paths['/ledger/voucher/{id}/attachment']['delete'];
+  const parameters = deleteIssue.parameters.map((parameter) =>
+    parameter.name === 'id'
+      ? {
+          ...parameter,
+          name: 'voucherId',
+        }
+      : {
+          ...parameter,
+        }
+  );
+  swaggerFile.paths['/ledger/voucher/{voucherId}/attachment']['delete'] = {
+    ...deleteIssue,
+    parameters,
+  };
+  delete swaggerFile.paths['/ledger/voucher/{id}/attachment'];
+  // console.log(swaggerFile.paths['/ledger/voucher/{voucherId}/attachment']);
 
-  Object.keys(swaggerFile.paths).map(url => {
-    //   .replace(/\{id\}/, 'BYID');
-    // console.log(pascalCase(safeUrlName));
-    const data = swaggerFile.paths[url];
-    Object.keys(data).map(func => {
-      let convertedName = convertName(url, func);
-      if (generatedOperationIds.includes(camelCase(convertedName)) && url.match(/\{/)) {
-        convertedName = convertName(url, func, true);
+  // Fix multiple
+  for (const key in swaggerFile.definitions) {
+    if (swaggerFile.definitions.hasOwnProperty(key)) {
+      for (const propKey in swaggerFile.definitions[key].properties) {
+        const { readOnly, description, ...prop } = swaggerFile.definitions[key].properties[
+          propKey
+        ];
+        swaggerFile.definitions[key].properties[propKey] = prop;
       }
-      const name = camelCase(convertedName);
-      if (name === 'listEvent') {
-        // console.log(url, func);
-      }
-      generatedOperationIds.push(name);
-      swaggerFile.paths[url][func].operationId = name;
-    });
-  });
-  
+    }
+  }
+
+  // console.log(JSON.stringify(warnings, null, 2));
+  let validation;
+  for (let index = 0; index < 4; index++) {
+    validation = await validate(swaggerFile);
+    validation.warnings
+      .filter((warning) => warning.code === 'UNUSED_DEFINITION')
+      .map((warning) => {
+        if (warning.path[1] && swaggerFile.definitions[warning.path[1]]) {
+          delete swaggerFile.definitions[warning.path[1]];
+        } else {
+          console.log(warning.path);
+        }
+      });
+  }
+
+  validation = await validate(swaggerFile);
+  console.log(validation.warnings);
+
   fs.writeFileSync(`${__dirname}/../swagger-fixed.json`, JSON.stringify(swaggerFile, null, 2));
 })();
